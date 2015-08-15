c238315179071ff61c973cde9b5caf905c0ced82
Tahler <tyler@berryac.com>
2015-08-15 12:36:52 -0600

    card margins perfect for now

--- app/src/main/java/me/tyborg/gymlog/adapters/SessionAdapter.java
+++ app/src/main/java/me/tyborg/gymlog/adapters/SessionAdapter.java
@@ -1,9 +1,11 @@
 package me.tyborg.gymlog.adapters;
 
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import java.util.List;
